394a9a27e1d9d01bf4c3aa53663969db3d87fca9
Caleb Montemayor <calebmontemayor@gmail.com>
2025-06-08 19:32:23 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Adventure Builder</h1>
+      <p>This is your GPT-powered tool in development.</p>
+    </div>
+  );
+}
+
+export default App;
